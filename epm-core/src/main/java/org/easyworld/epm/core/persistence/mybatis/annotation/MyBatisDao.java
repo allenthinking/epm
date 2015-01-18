c6571c8c3e38c68845af1e18a75b54ea9449f457
Allen.Wang <allenthinking@gmail.com>
2015-01-18 22:05:34 +0800

    change the child project

--- epm-core/src/main/java/org/easyworld/epm/core/persistence/mybatis/annotation/MyBatisDao.java
+++ epm-core/src/main/java/org/easyworld/epm/core/persistence/mybatis/annotation/MyBatisDao.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 /**
  * 标识MyBatis的DAO,方便{@link org.mybatis.spring.mapper.MapperScannerConfigurer}的扫描。
  * 
- * @author Allen Wang
+ * @author Allen Wang 
  * @version 2013-8-28
  */
 @Retention(RetentionPolicy.RUNTIME)
